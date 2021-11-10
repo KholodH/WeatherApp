08435f5c0f50848ba248d131f4024398a8f63a0c
Kholod Almalki <Kholodhalmalki@gmail.com>
2021-11-11 00:45:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/Main.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp
+
+data class Main(
+    val feels_like: Double, // 24.16
+    val humidity: Int, // 69
+    val pressure: Int, // 1007
+    val temp: Double, // 23.91
+    val temp_max: Double, // 26.2
+    val temp_min: Double // 21.61
+)
